d82a131180d9df1d22d1d373c876a5fcc4510e8b
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-08 15:44:10 +0530

    added showbar()

--- main.cpp
+++ main.cpp
@@ -27,3 +27,16 @@ void pointer(int on);
 void restrictmouse(int x1,int y1,int x2,int y2);
 void check_keys(int x,int y);
 void getmouse(int *button,int *x,int *y);
+float freq[7] = {130.81, 146.83, 164.81, 174.61,196, 220, 246.94 } ;
+int n=0,a=4,backcolor=2,exitcode=1;
+void showbar(int t)
+{
+ if(t>65) t=65;
+ if(t<1) t=1;
+ textcolor(15);
+ for(int q=0;q<=t;t++)
+ {
+	gotoxy(3+q,4);
+	cprintf("Û");
+ }
+}
