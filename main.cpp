1b927764d053eb9d89d12d8c58dbcd2802440685
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-08 15:31:46 +0530

    added class piano

--- main.cpp
+++ main.cpp
@@ -5,3 +5,16 @@
 #define SHOW 1
 #define HIDE 2
 union REGS input,output;
+
+class piano
+{
+    public:int BIGKEY,MIDKEY,back,border;
+            piano()  //CONSTRUCTOR INITIALIZATION
+            {
+                BIGKEY=15;
+                MIDKEY=1;
+                back=7;
+                border=15
+            }
+}color;
+
