d1e91dc168104dbe05ade6f5ab27b5da05b37d8e
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-08 15:42:15 +0530

    added functions

--- main.cpp
+++ main.cpp
@@ -22,3 +22,8 @@ void drawpiano(int x,int y);
 int check_xy(int x, int y);
 void BOX(int c,int r,int c1,int r1,int col);
 int initmouse();
+void setupscreen();
+void pointer(int on);
+void restrictmouse(int x1,int y1,int x2,int y2);
+void check_keys(int x,int y);
+void getmouse(int *button,int *x,int *y);
